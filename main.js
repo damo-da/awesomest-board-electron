3edcb28770a7d6889c37d443a1206fdb779400cd
Damodar Dahal <underscoredam@gmail.com>
2017-05-01 23:09:51 -0500

    working electron app

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
 import { app, BrowserWindow } from 'electron';
+import connect from 'connect';
+import serveStatic from 'serve-static';
+
+const PORT = 34577;
 
 let mainWindow = null;
+
+connect()
+    .use(serveStatic('./node_modules/awesomest-board/dist'))
+    .listen(PORT, () => {
+        console.log('Server running on ', PORT, '...');
+    });
 
 app.on('window-all-closed', () => {
   if (process.platform != 'darwin') {
@@ -10,7 +20,7 @@ app.on('window-all-closed', () => {
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('http://localhost:'+PORT);
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
